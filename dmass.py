2dc7f22fd7141960f5eb846348bf6d44d383d44d
Mannu05 <88075081+Mannu05@users.noreply.github.com>
2021-07-28 11:20:42 +0530

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -44,4 +44,4 @@ async def send(ctx, *, content: str):
                 await client.say("DM can't Sent To : {} :x: ".format(member))
 
 
-client.run("YOUR BOT'S TOKEN HERE")                
+client.run("ODY5ODEwNDAyNjY4NzIwMTY4.YQDoEA.tUd9mV0cmRQwnJN4cqOVfrj29LY")                
